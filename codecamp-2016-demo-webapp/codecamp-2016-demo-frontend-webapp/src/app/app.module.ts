0ccf4e8f93a5f83e43f05a3e1af70ce1fc0a3f4b
Martin Zimmermann <mzimmermann@next-level-integration.com>
2016-10-15 11:15:20 +0200

    add modules dogs and register. Use router for navigation
    Change-Id: I5beedad4ce33c7c4e7c2082405fda5b9a2bdb49c

--- codecamp-2016-demo-webapp/codecamp-2016-demo-frontend-webapp/src/app/app.module.ts
+++ codecamp-2016-demo-webapp/codecamp-2016-demo-frontend-webapp/src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { DogsComponent } from './dogs/dogs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    DogsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+        { path: 'register', component: RegisterComponent },
+        { path: 'dogs', component: DogsComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ codecamp-2016-demo-webapp/codecamp-2016-demo-frontend-webapp/src/app/dogs/dogs.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { DogsComponent } from './dogs.component';
+
+describe('Component: Dogs', () => {
+  it('should create an instance', () => {
+    let component = new DogsComponent();
+    expect(component).toBeTruthy();
+  });
+});
